fb7464bd43cf41ae39e9246e86d55c58df196529
austinws <60244004+austinws@users.noreply.github.com>
2023-12-25 22:31:58 -0600

    Commit 4
    Styled the font for, I think, every page.
    
    Everything else is now a mess because I am tinkering with the styling and the JS.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-fetch('orgs.json')
+/*fetch('orgs.json')
   .then(response => response.json())
   .then(data => {
     const orgs = data; // Assuming the parsed data is an array
@@ -11,7 +11,25 @@ fetch('orgs.json')
   .catch(error => {
     // Handle any errors during fetching or parsing
   });
+*/
+//PART ONE OLD CODE
+/*
+  fetch('orgs.json')
+  .then(response => response.json())
+  .then(data => {
+    const organizations = data; // Assuming the parsed data is an array
+    organizations.forEach(organization => {
+      console.log(organization.name);
+      console.log(organization.description);
+      // ...
+    });
+  })
+  .catch(error => {
+    // ...
+  });
+*/
 
+/*
 const orgList = document.getElementById('orgList');
 
 orgs.forEach(orgs => {
@@ -22,3 +40,38 @@ orgs.forEach(orgs => {
     `;
     orgList.appendChild(orgItem);
 });
+*/
+//PART TWO OLD CODE
+/*const orgsList = document.getElementById('orgList');
+
+organizations.forEach(organization => {
+    const orgItem = document.createElement('li');
+    orgItem.innerHTML = `
+      <h3>${organization.name}</h3>
+      <p>${organization.description}</p>
+      <a href="${organization.website}">Visit Website</a>
+    `;
+    orgList.appendChild(orgItem);
+  });
+*/
+
+fetch('orgs.json')
+  .then(response => response.json())
+  .then(data => {
+    const orgData = data;
+
+    const orgList = document.getElementById('orgList');
+
+    organizations.forEach(organization => {
+      const orgItem = document.createElement('li');
+      orgItem.innerHTML = `
+        <h3>${organization.name}</h3>
+        <p>${organization.description}</p>
+        <a href="${organization.website}">Visit Website</a>
+      `;
+      orgList.appendChild(orgItem);
+    });
+  })
+  .catch(error => {
+    // Handle errors
+  });
